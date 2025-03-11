731b5ec2a5ccfe9f9ecb465cd8da5b8e75a654ea
Daize Dong <dzdong2019@gmail.com>
2025-03-11 18:13:32 +0800

    Add `dict_to_args_str`

--- operations/operation_dict.py
+++ operations/operation_dict.py
@@ -1,4 +1,29 @@
-from typing import List
+from typing import Any, Dict, List
+
+
+def dict_to_args_str(args_dict: Dict[str, Any]) -> str:
+    shell_args = []
+
+    for key, value in args_dict.items():
+        arg_name = "--" + key.replace("_", "-")
+
+        if value is None:
+            pass
+        elif isinstance(value, bool) and value:
+            shell_args.append(arg_name)
+        elif isinstance(value, (int, float, str)):
+            shell_args.append(f"{arg_name}")
+            shell_args.append(f"{value}")
+        elif isinstance(value, list):  # cat with ","
+            shell_args.append(f"{arg_name}")
+            shell_args.append(",".join(map(str, value)))
+        elif isinstance(value, dict):  # cat with ","
+            shell_args.append(f"{arg_name}")
+            shell_args.append(",".join(f"{k}={v}" for k, v in value.items()))
+        else:
+            raise TypeError(f"Unsupported type for argument '{key}': {type(value)}")
+
+    return " ".join(shell_args)
 
 
 def reverse_dict(input_dict, aggregate_same_results=True):
