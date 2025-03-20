04773931a33a64aa400b596f118a3860309548c9
Daize Dong <dzdong2019@gmail.com>
2025-03-20 11:13:40 +0800

    Add Copy & Move Operation

--- io.py
+++ io.py
@@ -1,6 +1,7 @@
 import csv
 import fnmatch
 import gzip
+import inspect
 import json
 import lzma
 import multiprocessing
@@ -19,25 +20,64 @@ def create_dir(dir, suppress_errors=False) -> bool:
             return False
     except Exception as e:
         if suppress_errors:
-            print(f"{e}\n(This exception have been suppressed and would not influence the program execution)")
+            print(f"Exception within `{inspect.currentframe().f_code.co_name}`: {e}")
             return False
         else:
             raise e
 
 
-def delete_file_or_dir(dir, suppress_errors=False) -> bool:
+def delete_file_or_dir(path, suppress_errors=False) -> bool:
     try:
-        if os.path.isfile(dir):
-            os.remove(dir)
+        if os.path.isfile(path):
+            os.remove(path)
             return True
-        elif os.path.exists(dir):
-            shutil.rmtree(dir)
+        elif os.path.exists(path):
+            shutil.rmtree(path)
             return True
         else:
             return False
     except Exception as e:
         if suppress_errors:
-            print(f"{e}\n(This exception have been suppressed and would not influence the program execution)")
+            print(f"Exception within `{inspect.currentframe().f_code.co_name}`: {e}")
+            return False
+        else:
+            raise e
+
+
+def copy_file_or_dir(source_path, target_dir, print_info=False, suppress_errors=False) -> bool:
+    try:
+        if os.path.exists(source_path):
+            create_dir(target_dir, suppress_errors=suppress_errors)
+            if os.path.isfile(source_path):
+                shutil.copy2(source_path, target_dir)
+            else:
+                shutil.copytree(source_path, os.path.join(target_dir, os.path.basename(source_path)), dirs_exist_ok=True)
+            if print_info:
+                print(f"Copied: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
+            return True
+        else:
+            return False
+    except Exception as e:
+        if suppress_errors:
+            print(f"Exception within `{inspect.currentframe().f_code.co_name}`: {e}")
+            return False
+        else:
+            raise e
+
+
+def move_file_or_dir(source_path, target_dir, print_info=False, suppress_errors=False) -> bool:
+    try:
+        if os.path.exists(source_path):
+            create_dir(target_dir, suppress_errors=suppress_errors)
+            shutil.move(source_path, target_dir)
+            if print_info:
+                print(f"Moved: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
+            return True
+        else:
+            return False
+    except Exception as e:
+        if suppress_errors:
+            print(f"Exception within `{inspect.currentframe().f_code.co_name}`: {e}")
             return False
         else:
             raise e
