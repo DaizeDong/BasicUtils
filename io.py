cbb0b9811e0474410321ec0ae1de5a5e178a5291
Daize Dong <dzdong2019@gmail.com>
2025-03-31 15:32:25 +0800

    Update io

--- io.py
+++ io.py
@@ -11,10 +11,12 @@ import shutil
 from typing import Dict, List, Union
 
 
-def create_dir(dir, suppress_errors=False) -> bool:
+def create_dir(dir, print_info=False, suppress_errors=False) -> bool:
     try:
         if not os.path.exists(dir):
             os.makedirs(dir)
+            if print_info:
+                print(f"Created dir: {dir}")
             return True
         else:
             return False
@@ -26,13 +28,16 @@ def create_dir(dir, suppress_errors=False) -> bool:
             raise e
 
 
-def delete_file_or_dir(path, suppress_errors=False) -> bool:
+def delete_file_or_dir(path, print_info=False, suppress_errors=False) -> bool:
     try:
-        if os.path.isfile(path):
-            os.remove(path)
-            return True
-        elif os.path.exists(path):
-            shutil.rmtree(path)
+        if os.path.exists(path):
+            is_file = os.path.isfile(path)
+            if is_file:
+                os.remove(path)
+            else:
+                shutil.rmtree(path)
+            if print_info:
+                print(f"Deleted {'file' if is_file else 'dir'}: {path}")
             return True
         else:
             return False
@@ -48,12 +53,13 @@ def copy_file_or_dir(source_path, target_dir, print_info=False, suppress_errors=
     try:
         if os.path.exists(source_path):
             create_dir(target_dir, suppress_errors=suppress_errors)
-            if os.path.isfile(source_path):
+            is_file = os.path.isfile(source_path)
+            if is_file:
                 shutil.copy2(source_path, target_dir)
             else:
                 shutil.copytree(source_path, os.path.join(target_dir, os.path.basename(source_path)), dirs_exist_ok=True)
             if print_info:
-                print(f"Copied: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
+                print(f"Copied {'file' if is_file else 'dir'}: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
             return True
         else:
             return False
@@ -69,9 +75,10 @@ def move_file_or_dir(source_path, target_dir, print_info=False, suppress_errors=
     try:
         if os.path.exists(source_path):
             create_dir(target_dir, suppress_errors=suppress_errors)
+            is_file = os.path.isfile(source_path)
             shutil.move(source_path, target_dir)
             if print_info:
-                print(f"Moved: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
+                print(f"Moved {'file' if is_file else 'dir'}: {source_path} -> {os.path.join(target_dir, os.path.basename(source_path))}")
             return True
         else:
             return False
